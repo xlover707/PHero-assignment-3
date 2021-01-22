04ec9d7f20815bcf0501318eb5c77e1a68200255
ER Faisal Maulabhi <xlover707@gmail.com>
2021-01-22 18:50:10 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,18 @@
+// 
+// kilometer To Meter Converting
+  
+  //   function kilometerToMeter(kilometer) {
+  //   var meter =kilometer*1000;
+  //   return meter;
+  // }
+
+  //   var kiloToMeterConvert= kilometerToMeter(2);
+  //   console.log(kiloToMeterConvert);
+
+  // Budget Calculator
+
+  function budgetCalculator(budgetTotal){
+    var watch=50;
+    var phone=100;
+    var laptop=500;
+  }
