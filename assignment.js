b3d978627cee9d1b803fb956c97989ee05a10809
ER Faisal Maulabhi <xlover707@gmail.com>
2021-01-22 19:53:28 +0600

    PHero Assignment 3

--- assignment.js
+++ assignment.js
@@ -1,18 +1,39 @@
-// 
+// https://github.com/xlover707/PHero-assignment-3
+
 // kilometer To Meter Converting
   
-  //   function kilometerToMeter(kilometer) {
-  //   var meter =kilometer*1000;
-  //   return meter;
-  // }
+    function kilometerToMeter(kilometer) {
+    var meter =kilometer*1000;
+    return meter;
+  }
 
-  //   var kiloToMeterConvert= kilometerToMeter(2);
-  //   console.log(kiloToMeterConvert);
+    var kiloToMeterConvert= kilometerToMeter(2);
+    console.log(kiloToMeterConvert);
 
   // Budget Calculator
 
-  function budgetCalculator(budgetTotal){
-    var watch=50;
-    var phone=100;
-    var laptop=500;
+    function budgetCalculator(budgetTotal){
+      var watch=50;
+      var phone=100;
+      var laptop=500;
+    }
+
+  
+  
+    // Mega Friend Find out by Array
+
+  function megaFriend(largestName){
+    var mega =largestName[0];
+    for (var i=0; i < largestName.length; i++){
+      var element =largestName[i];
+    if(element>mega){
+      mega=element;
+    }
+    
   }
+  return mega;
+    
+  }
+  
+  var output= megaFriend(['Nayeem', 'Faisal', 'Sharmin']);
+  console.log('Mega Friend: ', output);
